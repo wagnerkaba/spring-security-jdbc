0a425a85aa573a8f139f69c230933fbbdfa9c7f7
wagnerkaba <bom.link+github@gmail.com>
2021-12-26 15:13:10 -0300

    primeira implementação do SecurityConfiguration

---
+++ src/main/java/com/wagner/springsecurityjdbc/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package com.wagner.springsecurityjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("pass")
+                                .roles("USER")
+
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // ADMIN tem acesso a /admin
+        // USER & ADMIN possuem acesso a /user
+        // qualquer pessoa tem acesso ao root "/"
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        //TODO This PasswordEncoder is not secure.
+        // Instead use an adaptive one way function like BCryptPasswordEncoder, Pbkdf2PasswordEncoder, or SCryptPasswordEncoder.
+        // Even better use DelegatingPasswordEncoder which supports password upgrades. There are no plans to remove this support.
+        // It is deprecated to indicate that this is a legacy implementation and using it is considered insecure.
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
